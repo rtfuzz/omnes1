e92eb2d922447df9ff1b6b978d7260587b6c8cff
rtfuzz <rtfuzz@gmail.com>
2017-01-08 10:55:18 -0200

    app

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://www.apetitego.com.br/mobileapp/api",
+	'DialogDefaultTitle' : "ApetiteGO",
+	'pushNotificationSenderid' : "YOUR_ANDROID_PUSH_PROJECT_ID",
+	'facebookAppId' : "172997422108",
+	'APIHasKey' : "@La2483801317"
+};
